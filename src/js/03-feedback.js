3ee5e698fb45ab9546339bcfb757edc64d64be3e
Anna Samoilova <samoilova0299@gmail.com>
2023-06-02 14:14:04 +0200

    add feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const LOCAL_KEY = 'feedback-form-state';
+
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onTextAreaInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+const { email, message } = form.elements;
+let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY));
+reloadPage();
+
+function onTextAreaInput(e) {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(dataForm));
+  console.log(dataForm);
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log({ email: email.value, message: message.value });
+
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCAL_KEY);
+}
+
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email;
+    message.value = dataForm.message;
+  }
+}
